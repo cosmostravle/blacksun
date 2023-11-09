b28884f880704ea5b09512990313055c8095b0c7
xiehui <xw__2011@163.com>
2023-11-09 23:59:33 +0800

    构建子模块common

---
+++ common/src/main/java/com/xiehui/common/CommonApplication.java
@@ -0,0 +1,15 @@
+package com.xiehui.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+* 删除其余相关启动类，只保留一个
+* */
+@SpringBootApplication
+public class CommonApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(CommonApplication.class, args);
+//    }
+
+}
